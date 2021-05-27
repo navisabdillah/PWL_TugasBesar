fa0fba9759ab75f1aef089bb3a33f37be49f1806
= <sigfriedmetatron@gmail.com>
2021-05-27 07:37:05 +0700

    .

---
+++ database/migrations/2021_05_18_125239_create_restaurants_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->integer('Nama')->primary;
+            $table->string('Jenis', 50);
+            $table->string('Harga', 10);
+            $table->string('Stok', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
