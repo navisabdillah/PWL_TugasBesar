31a5b7b48dfc79c52b9af0e641e615f07de5a2a4
= <=>
2021-06-14 08:20:22 +0700

    Membuat Model Pegawai dan Barang

---
+++ app/Models/Barang.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+use Illuminate\Database\Eloquent\Model; // Model Eloquent
+
+class Barang extends Model
+{
+    protected $table="barangs"; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswas
+    public $timestamps= false;
+    protected $primaryKey = 'Id_Barang'; // Memanggil isi DB Dengan primarykey
+
+    protected $fillable = [
+        'Id_Barang',
+        'Kode_Barang',
+        'Nama_Barang',
+        'Kategori_barang',
+        'Harga',
+        'Qty',
+        
+        ];
+}
