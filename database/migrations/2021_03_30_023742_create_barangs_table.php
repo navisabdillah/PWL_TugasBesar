a201cd7a98868a8c66d9e71b28149b080112a641
= <=>
2021-06-14 08:21:20 +0700

    Database Barang

---
+++ database/migrations/2021_03_30_023742_create_barangs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->integer('Id_Barang')->primary;
+            $table->string('Kode_Barang', 50);
+            $table->string('Nama_Barang', 10);
+            $table->text('Kategori_barang');
+            $table->string('Harga');
+            $table->string('Qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barangs');
+    }
+}
