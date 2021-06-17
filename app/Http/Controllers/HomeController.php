91c7f3eb8aa01265fad7eacd01cffd3b60da2266
= <=>
2021-06-18 06:30:41 +0700

    HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,4 +10,13 @@ class HomeController extends Controller
     public function index(){
         return view('Home');
     }
+    public function pegawai(){
+        return view('pegawai');
+    }
+    public function kurir(){
+        return view('kurir');
+    }
+    public function transaksi(){
+        return view('transaksi');
+    }
 }
