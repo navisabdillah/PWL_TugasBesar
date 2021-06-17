4e1cdc32bf52dc90cd7fe3c492b552883d3fdcf2
= <=>
2021-06-18 06:33:03 +0700

    UserSeeder untuk Admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,12 +17,13 @@ class UserSeeder extends Seeder
         //
         User::truncate();
         User::create([
-            'name' => 'Admin Restaurant',
+            'name' => 'Admin Perusahaan Barang',
             'level' => 'admin',
             'email' => 'admin@admin.com',
             'password' => bcrypt ('12345'),
             'remember_token' => Str::random(60),
 
             ]);
+            
     }
 }
